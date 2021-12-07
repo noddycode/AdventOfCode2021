8420809bb66c7426857f91e0cce442079f4bb24a
LANCASTER\shopper <shopper@clarkinc.biz>
2021-12-07 00:45:34 -0500

    Add code for day 7 part 2

--- Day07/part2.py
+++ Day07/part2.py
@@ -3,9 +3,29 @@ import statistics
 with open("input.txt") as fin:
     crab_list = [int(x.strip()) for x in fin.read().split(',')]
 
-# The example target position of "2" matches the median of the example numbers, so let's try that
-target_pos = int(statistics.median(crab_list))
+# crab_list = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
 
-fuel = sum([abs(x - target_pos) for x in crab_list])
+# Summation of a series of natural numbers:
+# https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
+# So we just need to plug each fuel number into
+# (n(n+1))/2
+# Then we'll just brute force it and narrow down results until we find the smallest value
+def get_lowest(test_target, previous_best):
+    test_points = (test_target - 1, test_target+1)
+    results = []
+    # Get fuel cost on either side of the test point
+    for test_point in test_points:
+        sum = 0
+        for crab in crab_list:
+            dist = abs(crab - test_point)
+            sum += (dist * (dist + 1))/2
+        results.append(sum)
 
-print(target_pos, fuel)
+    if previous_best < min(results):
+        print(int(previous_best))
+    else:
+        get_lowest(test_points[results.index(min(results))], min(results))
+
+
+
+get_lowest(statistics.median(crab_list), float('inf'))
